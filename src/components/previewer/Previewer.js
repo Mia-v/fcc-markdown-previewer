6e6d6d99f3c03de6c20dda76d4c4e2f9ce9ddcaa
Mia-v <vasilyeva.mariya@gmail.com>
2021-10-29 11:43:57 +0200

    Remove unused code.

--- src/components/previewer/Previewer.js
+++ src/components/previewer/Previewer.js
@@ -1,15 +1,11 @@
 import marked from 'marked';
 import './Previewer.scss';
-import hljs from 'highlight.js';
+
 
 export const Previewer = (props) => {
   
   marked.setOptions({
-    breaks: true,
-    langPrefix: "hljs language-",
-    highlight: function(code) {
-      return hljs.highlightAuto(code).value;
-    }
+    breaks: true
   });
   
   return(
