2cede562066881625e5156da28c78c4ae2015b95
Mia-v <vasilyeva.mariya@gmail.com>
2021-10-29 11:55:25 +0200

    Updates

---
+++ precache-manifest.76f8492ddf956321b954dc28e31a8274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6e50ab8dc6db21ee4db25cb9b5a6ad",
+    "url": "/fcc-markdown-previewer/index.html"
+  },
+  {
+    "revision": "f98885c38b40e80d0e16",
+    "url": "/fcc-markdown-previewer/static/css/main.45563d9b.chunk.css"
+  },
+  {
+    "revision": "56457027b7c272efbe2f",
+    "url": "/fcc-markdown-previewer/static/js/2.d0a94e93.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/fcc-markdown-previewer/static/js/2.d0a94e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9aa7c06c0697529b0d",
+    "url": "/fcc-markdown-previewer/static/js/3.80a267a2.chunk.js"
+  },
+  {
+    "revision": "f98885c38b40e80d0e16",
+    "url": "/fcc-markdown-previewer/static/js/main.d07c94c4.chunk.js"
+  },
+  {
+    "revision": "b13241277130b39ef6b8",
+    "url": "/fcc-markdown-previewer/static/js/runtime-main.facff942.js"
+  }
+]);
